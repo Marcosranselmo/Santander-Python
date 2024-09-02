189575c0fe0eb532f39cbaf4c70606480ba123d9
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-09-02 05:07:56 -0300

    curso-python

---
+++ script.inicial.py
@@ -0,0 +1,21 @@
+idade = 61
+
+if idade <= 1:
+   print ("É um bebe")
+
+elif idade >=2 and idade <= 9:
+   print ("É uma criança")
+
+elif idade >= 9 and idade <= 14:
+   print ("É um adolescente")
+
+elif idade >= 15 and idade <= 17:
+   print ("É um jovem")
+
+elif idade >= 18 and idade <= 40:
+   print ("É um Adulto")
+
+elif idade >= 41 and idade <= 60:
+   print ("É um jovem Senhor")
+else:
+   print ("Terceira idade")
