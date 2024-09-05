5445c3ac85ee94635510cf636eb931eb4c7b3dc0
Marcos Roberto Anselmo <marcos.anselmo@hotmail.com>
2024-09-05 05:04:15 -0300

    curso-python

---
+++ loops.py
@@ -0,0 +1,31 @@
+# While
+
+contador = 1
+
+while contador <= 5:
+
+    print(contador * 2)
+    contador += 1
+
+# For
+
+"""
+for i in range(10):
+
+    if i % 2 == 0:
+        continue
+    print(i)
+
+"""
+frutas = ["maçã", "banana", "laranja"]
+
+for fruta in frutas:
+    print(fruta)
+
+
+
+
+
+
+
+
